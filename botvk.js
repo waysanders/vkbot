f31d8cd60c4642cc329ed5466ef6150fd1248bdb
waysanders <47700961+waysanders@users.noreply.github.com>
2019-08-01 15:27:26 +0500

    Update botvk.js

--- botvk.js
+++ botvk.js
@@ -5,13 +5,46 @@ let settings = process.env;
 if (fs.existsSync('./settings.json')) settings = require('./settings.json');
 
 const bot = new vk(settings.vktoken);
-let text;
+
 
 bot.startPolling(() => {
-  console.log('[BOT] Бот успешно запущен!');
+    console.log('[BOT] Бот успешно запущен!');
 });
 
-bot.on((answer) => {
-    let message = answer.message;
-    if (message.text == '!id') return answer.reply('Peer ID Беседы: ' + message.peer_id + ' | From ID ' + message.from_id);
+
+
+bot.on((answer) => { 
+   let message = answer.message; 
+    if (message.text == '/start') return answer.reply('[Sanders prod.®] Successful launch!\nPeer ID Беседы: ' + message.peer_id + ' | From ID ' + message.from_id); 
+    if (message.text == 'хуй') return answer.reply('[System anti-mat] Система анти-мат, не матерись, маме расскажу!');
+    if (message.text == 'блядь') return answer.reply('[System anti-mat] Система анти-мат, не матерись, маме расскажу!');
+    if (message.text == 'ахуел') return answer.reply('[System anti-mat] Система анти-мат, не матерись, маме расскажу!');
+    if (message.text == 'пизда') return answer.reply('[System anti-mat] Система анти-мат, не матерись, маме расскажу!');
+    if (message.text == 'пиздец') return answer.reply('[System anti-mat] Система анти-мат, не матерись, маме расскажу!');
+    if (message.text == 'долбаеб') return answer.reply('[System anti-mat] Система анти-мат, не матерись, маме расскажу!');
+    if (message.text == 'пиздабол') return answer.reply('[System anti-mat] Система анти-мат, не матерись, маме расскажу!');
 });
+    
+bot.command('+peer', (ctx) => {
+    ctx.reply(`Peer ID -  ${ctx.message.peer_id}`)
+});  
+
+bot.command('+from', (ctx) => {
+    ctx.reply(`From ID -  ${ctx.message.from_id}`)
+});  
+
+bot.command('+user', (ctx) => {
+    ctx.reply(`Users ID -  ${ctx.message.user_id}`)
+});  
+
+// bot.command('+payload', (ctx) => {
+//     ctx.reply(`Payload -  ${messages.getConversationMembers}`)
+// });  
+
+bot.command('+dz', (ctx) => {
+    ctx.reply(`тут будет наше дз`)
+});  
+
+bot.command('+schedule', (ctx) => {
+    ctx.reply(`тут будет наше расписание`)
+});  
