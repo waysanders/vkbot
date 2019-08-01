a2cbf941086da167802876c8056f99902df80f4c
waysanders <47700961+waysanders@users.noreply.github.com>
2019-08-02 01:59:17 +0500

    Update botvk.js

--- botvk.js
+++ botvk.js
@@ -31,6 +31,8 @@ bot.on((answer) => {
     if (message.text == '+peer') return answer.reply('[Info] Peer ID - ' + message.peer_id);
     if (message.text == '+from') return answer.reply('[Info] From ID - ' + message.from_id);
     if (message.text == '+user') return answer.reply('[Info] You ID - ' + message.user_id);
+    let text;
+    if (message.text == text) return answer.reply('[Error] Команды не найдено!');
 });
     
 // bot.command('+peer', (ctx) => {
